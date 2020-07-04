f1bd2604f4442e037bad896d80ff984bd4df9bac
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2020-07-04 22:45:07 +0100

    if no errors, say so

---
+++ test/prepare_results.py
@@ -0,0 +1,9 @@
+results_txt = os.path.join(
+    os.path.dirname(os.path.realpath(__file__)), "results.txt")
+
+with open(results_txt) as f:
+    results = f.read()
+
+if results == "":
+    with open(results_txt, "w") as f:
+        "No errors or issues were found in any game."
