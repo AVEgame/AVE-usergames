15ab10107b7e8884e56139f2e48c358942670d68
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2020-07-04 22:41:35 +0100

    write result to file and comment on github

--- test/test_games.py
+++ test/test_games.py
@@ -11,6 +11,11 @@ games_path = os.path.join(
 games = [os.path.join(games_path, filename)
          for filename in os.listdir(games_path) if filename[-4:] == ".ave"]
 
+results_txt = os.path.join(
+    os.path.dirname(os.path.realpath(__file__)), "results.txt")
+with open(results_txt, "w") as f:
+    pass
+
 
 @pytest.mark.parametrize('filename', games)
 def test_games_for_errors(filename):
@@ -24,13 +29,17 @@ def test_games_for_errors(filename):
     info.sort(key=lambda e: -e.error_value)
 
     if len(errors) > 0:
-        print("\n  " + str(len(errors)) + " errors(s) in " + filename)
-        for e in errors:
-            print(e)
+        with open(results_txt, "a") as f:
+            f.write(str(len(errors)) + " error(s) in " + filename + "\n")
+            for e in errors:
+                f.write("  " + str(e) + "\n")
+            f.write("\n")
     if len(info) > 0:
-        print("\n  " + str(len(info)) + " info(s) in " + filename)
-        for e in info:
-            print(e)
+        with open(results_txt, "a") as f:
+            f.write(str(len(info)) + " info(s) in " + filename + "\n")
+            for e in info:
+                f.write("  " + str(e) + "\n")
+            f.write("\n")
 
     # Assert that the worst error is a Warning or lower
     assert len(errors) == 0
